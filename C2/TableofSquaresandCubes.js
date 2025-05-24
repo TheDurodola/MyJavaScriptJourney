352918a4a4d28e0a62a9c812ba39aaeae575e1c2
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-05-24 20:54:18 +0100

    Create TableofSquaresandCubes.js

---
+++ C2/TableofSquaresandCubes.js
@@ -0,0 +1,23 @@
+console.log("number\tsquare\tcube")
+
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+let num4 = 4;
+let num5 = 5;
+let num6 = 6;
+let num7 = 7;
+let num8 = 8;
+let num9 = 9;
+let num10 = 10;
+
+console.log("%d	%d	%d",num1, num1*num1,num1*num1*num1);
+console.log("%d	%d	%d",num2, num2*num2,num2*num2*num2);
+console.log("%d	%d	%d",num3, num3*num3,num3*num3*num3);
+console.log("%d	%d	%d",num4, num4*num4,num4*num4*num4);
+console.log("%d	%d	%d",num5, num5*num5,num5*num5*num5);
+console.log("%d	%d	%d",num6, num6*num6,num6*num6*num6);
+console.log("%d	%d	%d",num7, num7*num7,num7*num7*num7);
+console.log("%d	%d	%d",num8, num8*num8,num8*num8*num8);
+console.log("%d	%d	%d",num9, num9*num9,num9*num9*num9);
+console.log("%d	%d	%d",num10, num10*num10,num10*num10*num10);
